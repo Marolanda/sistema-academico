69e3ff64e4d3fc583d8f6de75e7b7c7335d4cadd
Mariana Holanda <hola.m4ri@gmail.com>
2024-11-04 23:24:33 -0300

    Adicionando novas pastas 04.11

---
+++ src/funcoes.js
@@ -0,0 +1,23 @@
+/*1º Módulo: Lançamento de notas pelo professor
+ Será de responsabilidade de um desenvolvedor
+ O professor insere as notas de 1UP e 2UP, que devem ser números decimais entre
+0.0 e 10.0, com uma única casa decimal.
+*/
+
+/*2º Módulo: Cálculo da média
+ Será de responsabilidade de um segundo desenvolvedor.
+ Com base nas notas de 1UP e 2UP, a média do aluno é calculada usando a
+fórmula: 𝑚𝑒𝑑𝑖𝑎 = (1𝑈𝑃 + 2𝑈𝑃)/2
+ A média calculada deve ser um número entre 0.0 e 10.0. */
+
+/*3º módulo: Status do aluno
+ Com base na média calculada, o status do aluno é determinado de acordo com a
+seguinte regra de negócio:
+o Média < 4.0: "Reprovado por média"
+o Média >= 6.0: "Aprovado por média"
+o Média entre 4.0 e 6.0 (inclusivo 4.0 e exclusivo 6.0): "Aguardando a final"
+ O resultado deve ser exibido como uma mensagem formatada, contendo o status
+do aluno. */
+
+
+module.exports
---
+++ test/funcoes.test.js
@@ -0,0 +1,15 @@
+require
+/**Implementação do módulo 1: Módulo de Lançamento de Notas
+(lancamentoNotas.js):
+ Função lancarNota(nota1, nota2) que valida e armazena as notas de 1UP e
+2UP.
+ Teste: Validação de entradas e verificação de erros para notas fora do intervalo.*/
+
+/*Implementação do módulo 2: Módulo de cálculo de médias (calcularMedia.js)
+ Função calcularMedia(nota1, nota2) que recebe as notas validadas e
+calcula a média.
+ Teste: Cálculo correto da média com base em diferentes valores de nota. */
+
+/*Implementação do módulo 3: Módulo de status do aluno (exibirStatus.js)
+ Função obterStatus(media) que retorna o status do aluno com base na média.
+ Teste: Verificação de status com base em diferentes médias. */
